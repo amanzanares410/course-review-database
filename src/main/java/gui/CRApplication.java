26f629aab3d247bb6fa158176a4adc36d86fe4bb
Ashley Manzanares <fnv2vx@virginia.edu>
2023-05-03 13:51:56 -0400

    adding ability to change scenes in application class

--- src/main/java/gui/CRApplication.java
+++ src/main/java/gui/CRApplication.java
@@ -1,11 +1,36 @@
 package gui;
 
 import reviews.CreateTables;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
 
-public class CRApplication {
+import java.io.IOException;
+
+public class CRApplication extends Application{
+
+    private static Stage stg;
+
+    //Resource used to learn how to switch between scenes: https://www.youtube.com/watch?v=HBBtlwGpBek
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(CRApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(root, 550, 600);
+        primaryStage.setTitle("Course Review");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-//        CreateTables.createTables();
+        launch();
     }
 }
