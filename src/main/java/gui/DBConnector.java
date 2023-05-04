d50bb812451cb9cdc30085ca020c8da740b839e6
priscillatehrani <yvp9pc@virginia.edu>
2023-05-04 19:49:59 -0400

    db

--- src/main/java/gui/DBConnector.java
+++ src/main/java/gui/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
     private static final String DRIVER = "org.sqlite.JDBC";
-    private static final String DB_URL = "jdbc:sqlite:/Users/priscillatehrani/IdeaProjects/login?client_id=58566862bd2a5ff748fb&return_to=/login/oauth/login?client_id=58566862bd2a5ff748fb&return_to=/login/oauth/hw7-coursereview-qvp8hy-yvp9pc-fnv2vx/Reviews.sqlite3";
+    private static final String DB_URL = "jdbc:sqlite:Reviews.sqlite3";
 
     public static Connection connect() {
         Connection conn = null;
